7698284362aeb8a50d088e46f9ca12d618edab27
rvost <romanvost@gmail.com>
2025-06-14 00:19:06 +0300

    Initial commit

---
+++ src/BiSignUtils/Commands.fs
@@ -0,0 +1,16 @@
+﻿module Commands
+open System.IO
+open FSharp.SystemCommandLine
+
+let generateCmd =
+    let handler (name: string, length: uint) =
+        ()
+    
+    let name = Input.Argument<string>("name", "The name of the authority")
+    let length = Input.Option<uint>(["-l"; "--length"], uint 1024, "The length of private key (in bits)")
+
+    command "generate" {
+        description "Generate private key"
+        inputs (name, length)
+        setHandler handler
+    }
---
+++ src/BiSignUtils/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.CommandLine.Invocation
+open System.CommandLine.Help
+open FSharp.SystemCommandLine
+open Commands
+
+
+[<EntryPoint>]
+let main argv =
+    let showHelp (ctx: InvocationContext) =
+        let hc = HelpContext(ctx.HelpBuilder, ctx.Parser.Configuration.RootCommand, System.Console.Out)
+        ctx.HelpBuilder.Write(hc)
+        
+    let ctx = Input.Context() 
+    rootCommand argv {
+        description "Tool for working with keys and signatures for ArmA and DayZ addons."
+        inputs ctx
+        setHandler showHelp
+        addCommand generateCmd
+    }
