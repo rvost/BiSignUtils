b7218fbba9c1ccb1f3ae9baeba2ed704a3613d6a
rvost <romanvost@gmail.com>
2025-06-14 00:38:56 +0300

    feat: Add command to generate private key

--- src/BiSignUtils/Commands.fs
+++ src/BiSignUtils/Commands.fs
@@ -1,13 +1,17 @@
 ﻿module Commands
 open System.IO
 open FSharp.SystemCommandLine
+open BIS.Signatures
 
 let generateCmd =
-    let handler (name: string, length: uint) =
+    let handler (name: string, length: int) =
+        let key = BiPrivateKey.Generate(name, length)
+        use output = File.Create($"{name}.biprivatekey")
+        key.Write(output)
         ()
     
     let name = Input.Argument<string>("name", "The name of the authority")
-    let length = Input.Option<uint>(["-l"; "--length"], uint 1024, "The length of private key (in bits)")
+    let length = Input.Option<int>(["-l"; "--length"], 1024, "The length of private key (in bits)")
 
     command "generate" {
         description "Generate private key"
